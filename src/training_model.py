e1102fcae59d950780fc607e12338e12e6bddddf
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-07 23:29:12 -0500

    training_model: calling to the training function with previous xor_data_loader, loss_module and optimizer file defined

--- src/training_model.py
+++ src/training_model.py
@@ -11,3 +11,14 @@ xor_dataset = XORDataset(size=100)
 
 # plotting
 visualize_samples(xor_dataset.data, xor_dataset.label, file_name="xor_dataset.png")
+
+# data loader
+xor_data_loader = data.DataLoader(xor_dataset, batch_size=10, shuffle=True)
+
+# loss module & optimizer
+loss_module = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+# training model
+
+train_model(model, optimizer, xor_data_loader, loss_module)
