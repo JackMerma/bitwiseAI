a79e00892aa07c2b41480fab8951914d6d032739
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-08 00:23:06 -0500

    plot: adding new row with ones for not_dataset (one dimention)

--- src/plot.py
+++ src/plot.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 
 def visualize_samples(data, label, file_name="fig.png"):
 
+    r, c = data.shape
+
+    if c == 1:
+        data = torch.cat((data, torch.ones((r, 1), dtype=data.dtype)), dim=1)
+
     data0 = data[label == 0]
     data1 = data[label == 1]
 
