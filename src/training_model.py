671cdb5daae8931da39c5c1183c868c25a7b509f
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-07 23:38:53 -0500

    training_model: changing model name for xor model and saving it

--- src/training_model.py
+++ src/training_model.py
@@ -4,21 +4,24 @@ from plot import *
 torch.manual_seed(17)
 
 # creating model
-model = SimpleClassifier(num_inputs=2, num_hidden=4, num_outputs=1)
+xor_model = SimpleClassifier(num_inputs=2, num_hidden=4, num_outputs=1)
 
 # creating XOR dataset
-xor_dataset = XORDataset(size=100)
+xor_dataset = XORDataset(size=2500)
 
 # plotting
 visualize_samples(xor_dataset.data, xor_dataset.label, file_name="xor_dataset.png")
 
 # data loader
-xor_data_loader = data.DataLoader(xor_dataset, batch_size=10, shuffle=True)
+xor_data_loader = data.DataLoader(xor_dataset, batch_size=150, shuffle=True)
 
 # loss module & optimizer
 loss_module = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+optimizer = torch.optim.SGD(xor_model.parameters(), lr=0.1)
 
 # training model
+train_model(xor_model, optimizer, xor_data_loader, loss_module)
 
-train_model(model, optimizer, xor_data_loader, loss_module)
+# saving model
+state_dict = xor_model.state_dict()
+torch.save(state_dict, "models/xor_model.tar")
