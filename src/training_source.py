d76e36dcbfbf4746363beb45096b015dcd097f21
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-07 22:55:04 -0500

    training_source: XORDataset class defined using Dataset class

--- src/training_source.py
+++ src/training_source.py
@@ -32,3 +32,23 @@ class Dataset(data.Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.label[idx]
 
+
+class XORDataset(Dataset):
+
+    def __init__(self, size, std=0.1):
+        super().__init__(size, std)
+        self.generate_continuous_xor()
+
+    def generate_continuous_xor(self):
+        data = torch.randint(low=0, high=2, size=(self.size, 2), dtype=torch.float32)
+
+        # calculate XOR output using sum
+        label = (data.sum(dim=1) == 1).to(torch.long)
+        
+        # gaussian noise
+        data += self.std * torch.randn(data.shape)
+
+        # data & label
+        self.data = data
+        slef.label = label
+
