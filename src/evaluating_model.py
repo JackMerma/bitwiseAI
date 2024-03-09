e644eb277ed342846f899deeda9700f6be772b4b
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-08 22:50:28 -0500

    evaluating_model: removing load_models function

--- src/evaluating_model.py
+++ src/evaluating_model.py
@@ -1,24 +1,5 @@
 from training_source import *
 
-def load_models(xor_model_name, and_model_name, or_model_name, not_model_name):
-    # loading model
-    xor_static_dict = torch.load(f"models/{xor_model_name}")
-    and_static_dict = torch.load(f"models/{and_model_name}")
-    or_static_dict = torch.load(f"models/{or_model_name}")
-    not_static_dict = torch.load(f"models/{not_model_name}")
-
-    xor_model = SimpleClassifier(num_inputs=2, num_hidden=4, num_outputs=1)
-    and_model = SimpleClassifier(num_inputs=2, num_hidden=4, num_outputs=1)
-    or_model = SimpleClassifier(num_inputs=2, num_hidden=4, num_outputs=1)
-    not_model = SimpleClassifier(num_inputs=1, num_hidden=2, num_outputs=1)
-
-    xor_model.load_state_dict(xor_static_dict)
-    and_model.load_state_dict(and_static_dict)
-    or_model.load_state_dict(or_static_dict)
-    not_model.load_state_dict(not_static_dict)
-
-    return xor_model, and_model, or_model, not_model
-
 xor_model, and_model, or_model, not_model = load_models("xor_model.tar", "and_model.tar", "or_model.tar", "not_model.tar")
 
 # evaluation
