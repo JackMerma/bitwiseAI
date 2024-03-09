d05260487c6a6293a26b9867b2f1847f810ef265
Jackson Merma <jmermap@unsa.edu.pe>
2024-03-08 22:49:51 -0500

    model_source: changing file name (main) for model_source

---
+++ src/model_source.py
@@ -0,0 +1,29 @@
+import torch
+from training_source import load_models
+
+xor_model, and_model, or_model, not_model = load_models("xor_model.tar", "and_model.tar", "or_model.tar", "not_model.tar")
+
+def evaluate_operation(model, data):
+    model.eval()
+    with torch.no_grad():
+        data = data.unsqueeze(0)
+        pred = model(data)
+        pred = pred.squeeze(dim=1)
+        return torch.sigmoid(pred)
+        #return 1 if torch.sigmoid(pred) >= 0.5 else 0
+
+def XOR(input1, input2):
+    data = torch.tensor([input1, input2], dtype=torch.float32)
+    return evaluate_operation(xor_model, data)
+
+def AND(input1, input2):
+    data = torch.tensor([input1, input2], dtype=torch.float32)
+    return evaluate_operation(and_model, data)
+
+def OR(input1, input2):
+    data = torch.tensor([input1, input2], dtype=torch.float32)
+    return evaluate_operation(or_model, data)
+
+def NOT(input1):
+    data = torch.tensor([input1], dtype=torch.float32)
+    return evaluate_operation(not_model, data)
